1db0183b1369e249f210cb395e0a9ac54c8246bb
koreafrom <storyerp@gmail.com>
2026-05-21 08:45:03 +0900

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,1 +1,5 @@
-const supabaseUrl = ''; const supabaseKey = '';
+// Supabase Client Settings shared across the application
+const supabaseUrl = 'https://pppbhlpxbepqrbnpggib.supabase.co';
+const supabaseKey = 'sb_publishable_Jh0S8GT6Gud8JC18_vYsbQ_uAHXtZ8A';
+
+const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
