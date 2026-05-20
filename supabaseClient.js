5ba4ee02a3a6eaf052ed19d1043d1cd1c0bbeeb1
koreafrom <storyerp@gmail.com>
2026-05-21 08:23:13 +0900

    Add files via upload

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,5 @@
-// Supabase 서버 원격 연결 클라이언트 공통 설정
+// Supabase Client Settings shared across the application
 const supabaseUrl = 'https://pppbhlpxbepqrbnpggib.supabase.co';
 const supabaseKey = 'sb_publishable_Jh0S8GT6Gud8JC18_vYsbQ_uAHXtZ8A';
 
-// 글로벌 변수로 supabase 객체 선언
 const supabase = supabasejs.createClient(supabaseUrl, supabaseKey);
